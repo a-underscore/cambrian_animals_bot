9f974b0245ddced9b71691a1533df6b639f7358b
Brendan Bortz <brendansbortz@protonmail.com>
2023-05-17 19:53:24 -0400

    b

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use serenity::{
 use std::{collections::HashSet, env, time::Duration};
 use tokio::time;
 
-const URL: &str = "https://en.wikipedia.org/wiki/Special:RandomInCategory/Cambrian_animals";
+const URL: &str = "https://en.wikipedia.org/wiki/Special:RandomInCategory/Extinct_animals";
 
 #[group]
 #[commands(ping, animal)]
